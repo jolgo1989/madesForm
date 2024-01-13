883e58a68b7c89a7c3cd3893f248e945a1674db2
jolgo <jlgo60@hotmail.com>
2024-01-13 08:03:03 -0500

    Se agrego evento onSubmit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,6 +28,8 @@ const tipos = [
 ];
 
 const Form = () => {
+  const { register, handleSubmit } = useForm(); //Registrar cada input
+
   // Estado del valor seleccionado
   const [valorSeleccion, setValor] = useState(null);
 
@@ -75,185 +77,201 @@ const Form = () => {
       }}
       elevation={3}
     >
-      <form className="form">
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column", // Alinear los elementos en una fila
-            gap: 2,
-            //! flexWrap: "wrap", // Envolver a la siguiente línea si no hay espacio suficiente
-          }}
-        >
-          {/* Nombre */}
-          <TextField
-            id="nombre-input"
-            label="Nombre"
-            variant="outlined"
+      <form
+        onSubmit={handleSubmit(() => {
+          console.log("Formulario enviado"); // función que se pasa al evento onSubmit y maneja la lógica del envío del formulario (por eso no se actualiza)
+        })}
+      >
+        <div className="form">
+          <Box
             sx={{
-              width: "180%",
+              display: "flex",
+              flexDirection: "column", // Alinear los elementos en una fila
+              gap: 2,
+              //! flexWrap: "wrap", // Envolver a la siguiente línea si no hay espacio suficiente
             }}
-          />
+          >
+            {/* Nombre */}
+            <TextField
+              id="nombre-input"
+              label="Nombre"
+              variant="outlined"
+              sx={{
+                width: "180%",
+              }}
+              {...register("nombre")} //Todos los campos que devuelva la funcion register seran asignados a TextField
+            />
 
-          <TextField
-            id="apellido-input"
-            label="Apellido"
-            variant="outlined"
-            sx={{ width: "180%" }}
-          />
+            <TextField
+              id="apellido-input"
+              label="Apellido"
+              variant="outlined"
+              sx={{ width: "180%" }}
+              {...register("Apellido")}
+            />
 
-          {/* Tipo de documento */}
-          <Autocomplete
-            id="selector-tipo"
-            options={tipos}
-            getOptionLabel={(option) => option.label}
-            value={valorSeleccion}
-            onChange={handleChange}
-            sx={{}}
-            renderInput={(params) => (
+            {/* Tipo de documento */}
+            <Autocomplete
+              id="selector-tipo"
+              options={tipos}
+              getOptionLabel={(option) => option.label}
+              value={valorSeleccion}
+              onChange={handleChange}
+              sx={{}}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Tipo de documento"
+                  variant="outlined"
+                />
+              )}
+            />
+
+            {/* Número de documento */}
+            <TextField
+              id="numero-documento-input"
+              label="Número de documento"
+              variant="outlined"
+              value={numeroDocumento}
+              onChange={handleChangeNumeroDocumento}
+              sx={{ width: "180%" }}
+              {...register("numeroDocumento")}
+            />
+
+            <TextField
+              id="nombre-input"
+              label="Dirección"
+              variant="outlined"
+              sx={{
+                width: "180%",
+              }}
+              {...register("direccion")}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column", // Alinear los elementos en una fila
+              gap: 2,
+              //! flexWrap: "wrap", // Envolver a la siguiente línea si no hay espacio suficiente
+            }}
+          >
+            {/* Imagen */}
+            <div className="userIcon">
+              <FaRegUser />
+            </div>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row", // Alinear los elementos en una fila
+                gap: 2,
+              }}
+            >
               <TextField
-                {...params}
-                label="Tipo de documento"
+                id="telefono-fijo-input"
+                label="Teléfono fijo"
                 variant="outlined"
+                value={telefonoFijo}
+                onChange={handleChangeTelefonoFijo}
+                sx={{ width: "70%" }}
+                {...register("telefonoFijo")}
               />
-            )}
-          />
 
-          {/* Número de documento */}
-          <TextField
-            id="numero-documento-input"
-            label="Número de documento"
-            variant="outlined"
-            value={numeroDocumento}
-            onChange={handleChangeNumeroDocumento}
-            sx={{ width: "180%" }}
-          />
+              <TextField
+                id="celular-input"
+                label="Celular"
+                variant="outlined"
+                value={celular}
+                onChange={handleChangeCelular}
+                sx={{ width: "70%" }}
+                {...register("celular")}
+              />
+            </Box>
 
-          <TextField
-            id="nombre-input"
-            label="Dirección"
-            variant="outlined"
-            sx={{
-              width: "180%",
-            }}
-          />
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column", // Alinear los elementos en una fila
-            gap: 2,
-            //! flexWrap: "wrap", // Envolver a la siguiente línea si no hay espacio suficiente
-          }}
-        >
-          {/* Imagen */}
-          <div className="userIcon">
-            <FaRegUser />
-          </div>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row", // Alinear los elementos en una fila
-              gap: 2,
-            }}
-          >
-            <TextField
-              id="telefono-fijo-input"
-              label="Teléfono fijo"
-              variant="outlined"
-              value={telefonoFijo}
-              onChange={handleChangeTelefonoFijo}
-              sx={{ width: "70%" }}
-            />
-
-            <TextField
-              id="celular-input"
-              label="Celular"
-              variant="outlined"
-              value={celular}
-              onChange={handleChangeCelular}
-              sx={{ width: "70%" }}
-            />
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row", // Alinear los elementos en una fila
-              gap: 2,
-            }}
-          >
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={["DatePicker"]}>
-                <DatePicker label="Fecha de nacimiento" />
-              </DemoContainer>
-            </LocalizationProvider>
-
-            <FormControl>
-              <FormLabel
-                sx={{
-                  display: "flex",
-                  justifyContent: "center", // Alinear los elementos en una fila
-                }}
-                id="demo-radio-buttons-group-label"
-              >
-                Genero
-              </FormLabel>
-              <RadioGroup
-                sx={{
-                  display: "flex",
-                  flexDirection: "row", // Alinear los elementos en una fila
-                }}
-                aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="female"
-                name="radio-buttons-group"
-              >
-                <FormControlLabel
-                  value="female"
-                  control={<Radio />}
-                  label="Femenino"
-                />
-                <FormControlLabel
-                  value="male"
-                  control={<Radio />}
-                  label="Masculino"
-                />
-                <FormControlLabel
-                  value="other"
-                  control={<Radio />}
-                  label="Otro"
-                />
-              </RadioGroup>
-            </FormControl>
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row", // Alinear los elementos en una fila
-              gap: 2,
-            }}
-          >
-            <TextField
-              id="nombre-input"
-              label="Correo#1"
-              variant="outlined"
+            <Box
               sx={{
-                width: "180%",
+                display: "flex",
+                flexDirection: "row", // Alinear los elementos en una fila
+                gap: 2,
               }}
-            />
+            >
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={["DatePicker"]}>
+                  <DatePicker label="Fecha de nacimiento" />
+                </DemoContainer>
+              </LocalizationProvider>
 
-            <TextField
-              id="nombre-input"
-              label="Correo#2"
-              variant="outlined"
+              <FormControl>
+                <FormLabel
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center", // Alinear los elementos en una fila
+                  }}
+                  id="demo-radio-buttons-group-label"
+                >
+                  Genero
+                </FormLabel>
+                <RadioGroup
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row", // Alinear los elementos en una fila
+                  }}
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="female"
+                  name="radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="Femenino"
+                  />
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Masculino"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Otro"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Box>
+
+            <Box
               sx={{
-                width: "180%",
+                display: "flex",
+                flexDirection: "row", // Alinear los elementos en una fila
+                gap: 2,
               }}
-            />
+            >
+              <TextField
+                id="nombre-input"
+                label="Correo#1"
+                variant="outlined"
+                sx={{
+                  width: "180%",
+                }}
+                {...register("correo")}
+              />
+
+              <TextField
+                id="nombre-input"
+                label="Correo#2"
+                variant="outlined"
+                sx={{
+                  width: "180%",
+                }}
+                {...register("correoDos")}
+              />
+            </Box>
           </Box>
-        </Box>
+        </div>
+
+        <button type="submit">Enviar</button>
       </form>
     </Paper>
   );
