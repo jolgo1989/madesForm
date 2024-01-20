f5e11f9056047af6c885ee91202b2aa0333609bd
jolgo <jlgo60@hotmail.com>
2024-01-20 06:51:55 -0500

    Se documento date

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { Controller } from "react-hook-form";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-
 import dayjs from "dayjs";
-import "dayjs/locale/es"; // Importar libreria para cambiar idioma del calendario
+import "dayjs/locale/es"; // Establecer la localización en español
 
-const startOfQ12022 = dayjs("1900-01-01T00:00:00.000");
-const endOfQ12022 = dayjs("2099-12-31T23:59:59.999");
+// Crear instancias de objetos dayjs para las fechas de inicio y fin del primer trimestre de 2022
+const startOfQ11900 = dayjs("1900-01-01T00:00:00.000");
+const endOfQ12099 = dayjs("2099-12-31T23:59:59.999");
 
 const Date = ({ control, errors }) => {
-  const [error, setError] = React.useState(null);
+  const [error, setError] = useState(null);
 
-  const errorMessage = React.useMemo(() => {
+  const errorMessage = useMemo(() => {
     switch (error) {
       case "maxDate":
       case "minDate": {
@@ -30,35 +30,39 @@ const Date = ({ control, errors }) => {
   }, [error]);
 
   return (
-    <Controller
+    <Controller //Integrar el control del formulario (control) con componentes no controlados como el DatePicker
       control={control}
-      name="selectedDate"
+      name="selectedDate" // especifica el nombre del campo (name) y las
       defaultValue={null}
+      //Rules:reglas de validación.
       rules={{
         required: {
           value: true,
           message: "Campo obligatorio",
         },
       }}
-      render={({ field }) => (
-        <LocalizationProvider
+      render={(
+        { field } //Fiel :objeto que contiene varias propiedades necesarias para conectar el componente DatePicker
+      ) => (
+        <LocalizationProvider // Proporciona el contexto de localización para adaptadores de fechas como AdapterDayjs y configura el idioma (adapterLocale) a español
           dateAdapter={AdapterDayjs}
           adapterLocale="es" /*Aplicar libreria para cambiar idiama a español*/
         >
-          <DatePicker
+          <DatePicker // Componente de selección de fechas
             label="Fecha de nacimiento"
             {...field}
             onError={(newError) => setError(newError)}
             slotProps={{
               textField: {
-                helperText: errors.selectedDate
+                helperText: errors.selectedDate //  HelperText: mostrar un mensaje de ayuda o error
                   ? errors.selectedDate.message
                   : errorMessage,
                 error: !!errors.selectedDate || errorMessage,
               },
             }}
-            minDate={startOfQ12022}
-            maxDate={endOfQ12022}
+            // Especificar los límites mínimos y máximos de las fechas seleccionables.
+            minDate={startOfQ11900}
+            maxDate={endOfQ12099}
           />
         </LocalizationProvider>
       )}
