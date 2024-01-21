f044ec9cc47e0d1b8c508099b39177144e45c1e4
jolgo <jlgo60@hotmail.com>
2024-01-21 16:55:02 -0500

    Se modifico nombre de formPrueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Form from "./components/Form";
-import PruebaForm from "./components/PruebaForm";
 
 function App() {
   return (
     <div>
       <section>
         <Form />
-        <PruebaForm />
       </section>
     </div>
   );
